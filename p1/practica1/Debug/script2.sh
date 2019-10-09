e3c1a881e2ef0cd5578177f70056f5903033d400
Adrian <adlopor96@hotmail.com>
2019-10-09 09:34:18 +0200

    First commit practice 2

---
+++ p1/practica1/Debug/script2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_xor.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_xor.dat xor
+./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_sin.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_sin.dat sin
+./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_quake.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_quake.dat quake
+./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_parkinsons.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_parkinsons.dat parkinsons
+
+
--- practica1/Debug/script2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_xor.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_xor.dat xor
-./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_sin.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_sin.dat sin
-./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_quake.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_quake.dat quake
-./script1.sh /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/train_parkinsons.dat /home/adrian/Escritorio/IMC/basesDatosPr1IMC/dat/test_parkinsons.dat parkinsons
-
-
