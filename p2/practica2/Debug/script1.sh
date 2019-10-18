18b7271eb83e80844d64a5de3070f55f3d90d3b8
Adrián López Ortiz <p42loora@uco.es>
2019-10-18 17:21:05 +0200

    Finished!

---
+++ p2/practica2/Debug/script1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./practica2 -t $1 -T $2 -l 2 -h 100 -f 1 -s >$3.txt
---
+++ p2/practica2/Debug/script2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./script1.sh /home/adrian/Escritorio/IMC/p2/basesDatosPr2IMC/dat/train_xor.dat /home/adrian/Escritorio/IMC/p2/basesDatosPr2IMC/dat/test_xor.dat xor
