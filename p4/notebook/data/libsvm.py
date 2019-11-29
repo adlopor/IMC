beed5fe312abb2ac89a11311680500e5a58daf65
Adrian <adlopor96@hotmail.com>
2019-11-29 12:36:53 +0100

    doing p4

--- p4/notebook/data/libsvm.py
+++ p4/notebook/data/libsvm.py
@@ -10,19 +10,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#from sklearn.grid_search import GridSearchCV
+from sklearn import model_selection
 from sklearn import svm
+from sklearn import preprocessing
 
 # Cargar el dataset
-data = pd.read_csv('dataset1.csv',header=None)
+data = pd.read_csv('dataset3.csv',header=None)
 X = data.iloc[:,:-1].values
 y = data.iloc[:,-1].values
 
+
+scaler = preprocessing.StandardScaler ()
+X = scaler.fit_transform (X , y )
+# Partimos el conjunto en train y test
+x_train , x_test , y_train , y_test = model_selection.train_test_split(X , y , stratify =y , test_size =0.25 , random_state =256)
+
+Cs = np.logspace(-5, 15, num=11, base=2)
+Gs = np.logspace(-15, 3, num=9, base=2)
+optimo = GridSearchCV(estimator=svm_model, param_grid=dict(C=Cs,gamma=Gs), n_jobs=-1,cv=5)
+optimo.fit(X_train,y_train)
+print (optimo.score(X_test,y_test))
+
 # Entrenar el modelo SVM
-svm_model = svm.SVC(kernel='linear',C=10000)
-svm_model.fit(X, y)
+#svm_model = svm.SVC(kernel='rbf',C=2, gamma=2)
+#svm_model.fit(x_train, y_train)
 
 #Para obtener el CCR (pregunta 3).
-print("CCR: " ,svm_model.score(X,y))
+print("CCR: " ,svm_model.score(x_train,y_train))
 
 # Representar los puntos
 plt.figure(1)
